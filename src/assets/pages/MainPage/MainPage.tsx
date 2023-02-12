feed04a0058206c4238a634a8c1cc4f45a304f62
XenoPOMP <101574433+XenoPOMP@users.noreply.github.com>
2023-02-12 13:44:24 +0300

    add ReactQuery

--- src/assets/pages/MainPage/MainPage.tsx
+++ src/assets/pages/MainPage/MainPage.tsx
@@ -1,6 +1,12 @@
 import Page from '@components/Page/Page';
+import { useQuery } from 'react-query';
+import { BitcoinCurrencyService } from '@services/BitcoinCurrency.service';
 
 const MainPage = () => {
+  const { isLoading, data, error } = useQuery('bitcoin currency', () =>
+    BitcoinCurrencyService.getBitcoinCurrency(),
+  );
+
   return (
     <Page
       meta={{ pageTitle: 'Main', pageDescription: '', keywords: '' }}
--- src/main.tsx
+++ src/main.tsx
@@ -4,11 +4,22 @@ import App from './App';
 import './main.scss'
 import { Provider as ReduxProvider } from 'react-redux';
 import { store } from '@redux/index';
+import { QueryClient, QueryClientProvider } from "react-query";
+
+const queryClient = new QueryClient({
+  defaultOptions: {
+    queries: {
+      refetchOnWindowFocus: true
+    },
+  }
+});
 
 ReactDOM.createRoot(document.getElementById('root') as HTMLElement).render(
   <React.StrictMode>
-    <ReduxProvider store={store}>
-      <App />
-    </ReduxProvider>
+    <QueryClientProvider client={queryClient}>
+      <ReduxProvider store={store}>
+        <App />
+      </ReduxProvider>
+    </QueryClientProvider>
   </React.StrictMode>,
 )
