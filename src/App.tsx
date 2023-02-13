ae6e2c7321f4822d91b2254ddc17a577e954f380
XenoPOMP <101574433+XenoPOMP@users.noreply.github.com>
2023-02-13 16:54:25 +0300

    add useBoolean hook

--- src/App.tsx
+++ src/App.tsx
@@ -1,8 +1,6 @@
 import { BrowserRouter as Router, Routes, Route } from 'react-router-dom';
 import MainPage from '@pages/MainPage/MainPage';
 import NotFound from '@pages/NotFound/NotFound';
-import HookTestingPage from '@pages/Testing/HookTesting/HookTestingPage';
-import ReduxTestingPage from '@pages/Testing/ReduxTesting/ReduxTestingPage';
 
 function App() {
   return (
@@ -11,12 +9,6 @@ function App() {
         <Route path={'*'} element={<NotFound />} />
 
         <Route path={'/'} element={<MainPage />} />
-
-        <Route path={'/testing'}>
-          <Route path={'hooks'} element={<HookTestingPage />} />
-
-          <Route path={'redux'} element={<ReduxTestingPage />} />
-        </Route>
       </Routes>
     </Router>
   );
