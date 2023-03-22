2b2625d7796062818b29770b3bbbc28cd9e7f955
XenoPOMP <101574433+XenoPOMP@users.noreply.github.com>
2023-03-22 19:36:55 +0300

    update useEnv hook

--- src/assets/hooks/useEnv.ts
+++ src/assets/hooks/useEnv.ts
@@ -1,26 +1,16 @@
 export type TestingMode = 'FRONTEND' | 'BACKEND' | 'FULLSTACK';
 
-export type Env = {
-  API_URL?: string,
-  TESTING_MODE?: TestingMode,
-};
+export interface Env {
+  API_URL?: string;
+  TESTING_MODE?: TestingMode;
+}
 
 const useEnv = (): Env => {
   const env = import.meta.env;
 
-  const fetchFromEnv = (item: any): string => {
-    const result = JSON.stringify(item);
-
-    // prettier-ignore
-    return result
-      .replace(/^"/i, '')
-      .replace(/"$/i, '');
-  };
-
   return {
-    API_URL: fetchFromEnv(env.VITE_API_URL),
-    // @ts-ignore
-    TESTING_MODE: fetchFromEnv(env.VITE_TESTING_MODE),
+    API_URL: env.VITE_API_URL,
+    TESTING_MODE: env.VITE_TESTING_MODE,
   };
 };
 
