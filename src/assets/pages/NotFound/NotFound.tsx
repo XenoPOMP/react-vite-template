9dd1e7303e185574bb41288d528f99df70f6c38d
XenoPOMP <101574433+XenoPOMP@users.noreply.github.com>
2023-04-03 22:42:09 +0300

    refactor NotFound page

--- src/assets/pages/NotFound/NotFound.tsx
+++ src/assets/pages/NotFound/NotFound.tsx
@@ -1,5 +1,6 @@
 import React from 'react';
 import Page from '@components/Page/Page';
+
 const NotFound = () => {
   return (
     <Page meta={{ pageTitle: '404', pageDescription: '', keywords: '' }}>
