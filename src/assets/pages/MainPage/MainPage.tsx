33aa7c9f92dbf1bfbba0926b3a7113f3151c58bb
XenoPOMP <101574433+XenoPOMP@users.noreply.github.com>
2023-04-21 22:07:43 +0300

    prettier code clean up

--- src/assets/pages/MainPage/MainPage.tsx
+++ src/assets/pages/MainPage/MainPage.tsx
@@ -1,13 +1,15 @@
-import Page from '@components/Page/Page';
-import styles from './MainPage.module.scss';
 import cn from 'classnames';
 
+import Page from '@components/Page/Page';
+
+import styles from './MainPage.module.scss';
+
 const MainPage = () => {
-  return (
-    <Page
-      meta={{ pageTitle: 'Main', pageDescription: '', keywords: '' }}
-    ></Page>
-  );
+	return (
+		<Page
+			meta={{ pageTitle: 'Main', pageDescription: '', keywords: '' }}
+		></Page>
+	);
 };
 
 export default MainPage;
