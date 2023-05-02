71f1e2a00260b9c9cd1a06944b22989c3e31b667
XenoPOMP <101574433+XenoPOMP@users.noreply.github.com>
2023-05-02 19:58:28 +0300

    add smooth-scroll function

---
+++ src/assets/utils/smooth-scroll.ts
@@ -0,0 +1,25 @@
+import { Easing } from 'framer-motion';
+import { scroller } from 'react-scroll';
+
+import ArgumentError from '@errors/ArgumentError';
+
+export const smoothScroll = (
+	id: string,
+	props?: {
+		duration?: number;
+		delay?: number;
+		ease?: Easing;
+		offset?: number;
+	}
+) => {
+	if (/^.+#/i.test(id)) {
+		throw new ArgumentError(`wrong format of id (expected #... or ...`);
+	}
+
+	scroller.scrollTo(id.replace(/^#/i, ''), {
+		duration: props?.duration ? props?.duration : 1000,
+		delay: props?.delay ? props?.delay : 0,
+		smooth: props?.ease ? props?.ease : 'easeOut',
+		offset: props?.offset ? props?.offset : -50,
+	});
+};
