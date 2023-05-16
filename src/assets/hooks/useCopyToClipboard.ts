4c9165db79b5e63cfc3f725623d0cf4aaa4c2764
XenoPOMP <101574433+XenoPOMP@users.noreply.github.com>
2023-05-16 20:21:17 +0300

    create useCopyToClipboard

---
+++ src/assets/hooks/useCopyToClipboard.ts
@@ -0,0 +1,36 @@
+import { useCallback, useEffect } from 'react';
+
+import useBoolean from '@hooks/useBoolean';
+
+const useCopyToClipboard = (): {
+	isCopied: boolean;
+	copy: (text: string) => void;
+} => {
+	const [isCopied, _, setIsCopied] = useBoolean(false);
+
+	const copy = useCallback((text: string) => {
+		navigator.clipboard
+			.writeText(text)
+			.then(() => {
+				setIsCopied(true);
+			})
+			.catch(err => {
+				console.error(`Unable to copy text: ${err}`);
+			});
+	}, []);
+
+	useEffect(() => {
+		if (isCopied) {
+			const timer = setTimeout(() => setIsCopied(false), 3000);
+
+			return () => clearTimeout(timer);
+		}
+	}, [isCopied]);
+
+	return {
+		isCopied,
+		copy,
+	};
+};
+
+export default useCopyToClipboard;
