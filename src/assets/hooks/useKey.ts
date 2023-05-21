b5c3a8344f101f7df9d529df12008a9c805862cc
XenoPOMP <101574433+XenoPOMP@users.noreply.github.com>
2023-05-21 18:50:19 +0300

    update useKey hook

--- src/assets/hooks/useKey.ts
+++ src/assets/hooks/useKey.ts
@@ -13,13 +13,24 @@ import { KeyCodes } from '@type/KeyCodes';
  * @param key                                         target key code.
  * @param {Parameters<typeof useKey>[1]} callback			callback that will be called
  * 																										in key press moment.
+ * @param options                                     hook options.
  */
 export const useKey = (
 	// key: Lowercase<keyof typeof Key>,
 	key: keyof typeof KeyCodes,
-	callback: (event?: KeyboardEvent) => any
+	callback: (event?: KeyboardEvent) => any,
+	options?: {
+		control?: boolean;
+		shift?: boolean;
+	}
 ) => {
 	const callbackRef = useRef<Parameters<typeof useKey>[1]>(callback);
+
+	const defaultOptions: Parameters<typeof useKey>[2] = {
+		control: false,
+		shift: false,
+		...options,
+	};
 
 	useEffect(() => {
 		callbackRef.current = callback;
@@ -27,7 +38,11 @@ export const useKey = (
 
 	useEffect(() => {
 		const handle = (event: KeyboardEvent) => {
-			if (event.code === KeyCodes[key]) {
+			if (
+				event.code === KeyCodes[key] &&
+				defaultOptions.control === event.ctrlKey &&
+				defaultOptions.shift === event.shiftKey
+			) {
 				callbackRef.current(event);
 			}
 		};
