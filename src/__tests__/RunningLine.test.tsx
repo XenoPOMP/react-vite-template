08b3691fc27cdac2de6f4a5eb7b1dc31fc31916b
XenoPOMP <101574433+XenoPOMP@users.noreply.github.com>
2023-05-25 17:54:54 +0300

    write tests

---
+++ src/__tests__/RunningLine.test.tsx
@@ -0,0 +1,18 @@
+import { render } from '@testing-library/react';
+import { describe, expect, test } from 'vitest';
+
+import RunningLine from '@ui/RunningLine/RunningLine';
+
+import skipTestCondition from '@utils/skipTestCondition';
+
+describe.skipIf(skipTestCondition('FRONTEND'))('Running line component', () => {
+	test('Match snapshot', () => {
+		expect(
+			render(<RunningLine verticalAlign>Some line content</RunningLine>)
+		).toMatchSnapshot();
+
+		expect(
+			render(<RunningLine>Some line content</RunningLine>)
+		).toMatchSnapshot();
+	});
+});
