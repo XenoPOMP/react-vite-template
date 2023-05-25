7d168cc339cb2a28e1f7559c2e9c317022e45ceb
XenoPOMP <101574433+XenoPOMP@users.noreply.github.com>
2023-05-25 17:41:36 +0300

    write few tests

---
+++ src/__tests__/DragMenu.test.tsx
@@ -0,0 +1,45 @@
+import { describe, expect, test } from 'vitest';
+
+import DragMenu from '@ui/DragMenu/DragMenu';
+
+import renderWithProviders from '@utils/renderWithProviders';
+import skipTestCondition from '@utils/skipTestCondition';
+
+describe.skipIf(skipTestCondition('FRONTEND'))('Draggable menu', () => {
+	test('Match snapshot', () => {
+		expect(
+			renderWithProviders(
+				<DragMenu axis={'x'}>
+					<div>Drag me</div>
+				</DragMenu>
+			)
+		).toMatchSnapshot();
+
+		expect(
+			renderWithProviders(
+				<DragMenu
+					axis={'x'}
+					classNames={{
+						constraint: 'parent class',
+					}}
+				>
+					<div>Drag me</div>
+				</DragMenu>
+			)
+		).toMatchSnapshot();
+
+		expect(
+			renderWithProviders(
+				<DragMenu
+					axis={'x'}
+					classNames={{
+						constraint: 'parent class',
+						draggable: 'child class',
+					}}
+				>
+					<div>Drag me</div>
+				</DragMenu>
+			)
+		).toMatchSnapshot();
+	});
+});
