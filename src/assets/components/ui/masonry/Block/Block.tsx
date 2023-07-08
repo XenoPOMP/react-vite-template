07250f7e7b338af43cc72ee93f68a461da88375c
XenoPOMP <101574433+XenoPOMP@users.noreply.github.com>
2023-07-08 22:02:09 +0300

    create Masonry component

---
+++ src/assets/components/ui/masonry/Block/Block.tsx
@@ -0,0 +1,28 @@
+import cn from 'classnames';
+import { FC } from 'react';
+
+import { PropsWith } from '@type/PropsWith';
+
+import styles from './Block.module.scss';
+import type { BlockProps } from './Block.props';
+
+/**
+ * See docs in {@link Masonry}.
+ *
+ * @param className
+ * @param style
+ * @param id
+ * @param children
+ * @constructor
+ */
+const Block: FC<
+	PropsWith<'className' | 'style' | 'id' | 'children', BlockProps>
+> = ({ className, style, id, children }) => {
+	return (
+		<div className={cn(styles.block, className)} id={id} style={style}>
+			{children}
+		</div>
+	);
+};
+
+export default Block;
---
+++ src/assets/components/ui/masonry/Column/Column.tsx
@@ -0,0 +1,28 @@
+import cn from 'classnames';
+import { FC } from 'react';
+
+import { PropsWith } from '@type/PropsWith';
+
+import styles from './Column.module.scss';
+import type { ColumnProps } from './Column.props';
+
+/**
+ * See docs in {@link Masonry}.
+ *
+ * @param children
+ * @param className
+ * @param style
+ * @param id
+ * @constructor
+ */
+const Column: FC<
+	PropsWith<'className' | 'style' | 'id' | 'children', ColumnProps>
+> = ({ children, className, style, id }) => {
+	return (
+		<div className={cn(styles.column, className)} id={id} style={style}>
+			{children}
+		</div>
+	);
+};
+
+export default Column;
