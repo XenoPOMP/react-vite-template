139954c55af651684be8a3cc03086f15e007191d
XenoPOMP <101574433+XenoPOMP@users.noreply.github.com>
2023-07-13 18:19:58 +0300

    useAppSettings enhancements

--- src/assets/hooks/useAppSettings.ts
+++ src/assets/hooks/useAppSettings.ts
@@ -1,6 +1,6 @@
-import { useSelector } from 'react-redux';
-import { useAppSelector } from '@redux/hooks';
+import { useAppDispatch, useAppSelector } from '@redux/hooks';
 import { AppSettings } from '@redux/reducers/appSettingsSlice';
+import { DeepPartial } from 'redux';
 
 type SettingController<T> = {
   get: () => T;
@@ -11,7 +11,8 @@ type SettingController<T> = {
     }
   : {});
 
-interface IUseAppSettings {
+interface IUseAppSettings
+  extends Record<keyof AppSettings, DeepPartial<SettingController<any>>> {
   appVersion: Omit<SettingController<AppSettings['appVersion']>, 'set'>;
   appName: Omit<SettingController<AppSettings['appName']>, 'set'>;
 }
@@ -20,6 +21,8 @@ const useAppSettings = (): IUseAppSettings => {
   const { appVersion, appName }: AppSettings = useAppSelector(
     state => state.appSettings,
   );
+
+  const dispatch = useAppDispatch();
 
   return {
     appVersion: {
