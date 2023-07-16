b4ed75f265d2ebc3c43d95e9bfbea7c4d7c839ed
XenoPOMP <101574433+XenoPOMP@users.noreply.github.com>
2023-07-16 04:16:17 +0300

    write new tests

--- src/assets/pages/TestingPage/TestingPage.tsx
+++ src/assets/pages/TestingPage/TestingPage.tsx
@@ -1,15 +1,37 @@
-import { FC } from 'react';
+import { FC, useContext, useEffect } from 'react';
+
+import { BodyClassnameContext } from '@providers/BodyClassnameProvider/BodyClassnameProvider';
 
 import useAppSettings from '@hooks/useAppSettings';
 
 import { TestingPageProps } from './TestingPage.props';
 
 const TestingPage: FC<TestingPageProps> = ({}) => {
-  const { appVersion } = useAppSettings();
+  const { appVersion, language } = useAppSettings();
+  const { classes, registerClasses, deleteClasses } =
+    useContext(BodyClassnameContext);
+
+  useEffect(() => {
+    registerClasses('test', ['testing']);
+
+    console.log(classes);
+
+    return () => {
+      deleteClasses('test');
+    };
+  });
 
   return (
     <div>
-      <>App version: {appVersion.get()}</>
+      <p>App version: {appVersion.get()}</p>
+
+      <p id={'lang-output'}>Language: {language.get()}</p>
+      <button className={'lang-switch'} onClick={() => language.set('ru')}>
+        Change lang to Russian
+      </button>
+      <button className={'lang-switch'} onClick={() => language.set('en')}>
+        Change lang to English
+      </button>
     </div>
   );
 };
--- src/assets/redux/__tests__/appSettingsSlice.test.ts
+++ src/assets/redux/__tests__/appSettingsSlice.test.ts
@@ -1,7 +1,11 @@
 import { describe, expect, test } from 'vitest';
+
 import appSettingsSlice, {
+  AppSettings,
+  changeLang,
   initialAppSettings,
 } from '@redux/reducers/appSettingsSlice';
+import { ReduxAction } from '@redux/types';
 
 import skipTestCondition from '@utils/skipTestCondition';
 
@@ -9,9 +13,21 @@ describe.skipIf(skipTestCondition('FRONTEND'))('Redux => AppSettings', () => {
   test('should return default state when passed an empty action', () => {
     const result = appSettingsSlice(undefined, { type: '' });
 
-    expect(result).toEqual({
+    expect(result).toStrictEqual({
       appVersion: '0.0.0',
       appName: 'React Vite Application',
+      language: 'en',
     });
+  });
+
+  test('change language with "changeLang" action', () => {
+    const action: ReduxAction<AppSettings['language']> = {
+      type: changeLang.type,
+      payload: 'ru',
+    };
+
+    const result = appSettingsSlice(undefined, action);
+
+    expect(result.language).toBe('ru');
   });
 });
