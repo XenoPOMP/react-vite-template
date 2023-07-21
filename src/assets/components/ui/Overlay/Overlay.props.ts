d064a1db5ab3b7781978fd624d277347b90a2333
XenoPOMP <101574433+XenoPOMP@users.noreply.github.com>
2023-07-21 18:04:53 +0300

    update overlay component

--- src/assets/components/ui/Overlay/Overlay.props.ts
+++ src/assets/components/ui/Overlay/Overlay.props.ts
@@ -1,8 +1,17 @@
-export interface OverlayProps {
+import { Modify } from '@type/Modify';
+import { PropsWith } from '@type/PropsWith';
+
+export interface OverlayProps
+  extends Modify<
+    PropsWith<'children' | 'className' | 'id', {}>,
+    'id',
+    Required<PropsWith<'children' | 'className' | 'id', {}>['id']>
+  > {
   backdrop: {
     blurAmount?: number;
     blurColor?: string;
   };
   trigger: boolean;
   flexCenter?: boolean;
+  blockScroll?: boolean;
 }
